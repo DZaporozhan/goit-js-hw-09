ed5f9c56e485b17643c74cd01b20a146c8ec90e8
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-08-11 14:03:55 +0300

    Задача №1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onBgColor);
+refs.stopBtn.addEventListener('click', stopBgColor);
+
+let timerId = null;
+
+refs.stopBtn.setAttribute('disabled', 'disabled');
+
+function onBgColor(evt) {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  refs.stopBtn.removeAttribute('disabled');
+}
+
+function stopBgColor(evt) {
+  clearInterval(timerId);
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}

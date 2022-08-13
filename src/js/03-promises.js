9a327bac473624b6fe44fe962b28bd1f071b5e2e
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-08-13 20:30:12 +0300

    Задача 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+const refs = {
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  submit: document.querySelector('.form button'),
+};
+
+const delay = refs.delay.addEventListener('input', evn => {
+  console.log(evn.target.value);
+  return evn.target.value;
+});
+const step = refs.step.addEventListener('input', evn => {
+  console.log(evn.target.value);
+  return evn.target.value;
+});
+const amount = refs.amount.addEventListener('input', evn => {
+  console.log(evn.target.value);
+  return evn.target.value;
+});
+
+refs.submit.addEventListener('submit', e => {
+  e.preventDefault();
+  for (let i = 0; i < amount; i++) {
+    createPromise();
+  }
+  createPromise();
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve('Success! Value passed to resolve function');
+      } else {
+        reject('Error! Error passed to reject function');
+      }
+    }, delay);
+    return promise;
+  });
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
